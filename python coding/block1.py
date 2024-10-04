3eddb3cfb8df54c79ed88070196bf905355664c9
Abdiabera <91797484+Abdiabera@users.noreply.github.com>
2024-10-04 03:03:38 -0400

    Delete python coding directory

--- python coding/block1.py
+++
@@ -1,7 +0,0 @@
-filename = 'data/pi_digits.text'
-pi_String = ''
-with open (filename) as f:
-for line in f:
-pi_String  += line.strip()
-print(pi_String)
-print(len(filename))
--- python coding/phone number.py/Phone number.py/phone number.py
+++
@@ -1,5 +0,0 @@
-import phonenumbers
-from test import number
-from phonenumbers import geocoder
-ch_nmber = phonenumbers.parse(numbers,"CH")
-print(geocoder.description_for_number(ch_nmber, "en"))
